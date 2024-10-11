1347e6a5236a8fdc98355ec6b37fb6ae312a341f
TrongNhan <trongnhan0703it@gmail.com>
2024-10-11 11:36:21 +0700

    feat: init project

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import configViewEngine from './configs/viewEngine.config';
+import router from './routes/web';
+import { configDotenv } from 'dotenv';
+configDotenv();
+const app = express();
+configViewEngine(app);
+
+
+app.use('/', router);
+
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log('>>>>>app is running successfully on the port: ' + PORT)
+})

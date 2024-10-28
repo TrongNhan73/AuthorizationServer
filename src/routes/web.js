b91e309309fc5ba61db4c821f5ce822b80a55dc6
TrongNhan73 <trongnhan0703it@gmail.com>
2024-10-28 19:54:46 +0700

    feat(test):create test api

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getHomePage, getUser, handleCreateUser, handleDeleteUser, handleUpdateUser, handleGetUpdatePage } from '../controllers/user.controller';
-
+import { testApi } from '../controllers/apiController';
 
 const router = express.Router();
 
@@ -13,5 +13,6 @@ router.post('/users/create-user', handleCreateUser);
 router.post('/users/delete-user/:id', handleDeleteUser);
 router.post('/users/update-user-page/:id', handleGetUpdatePage);
 router.post('/users/update-user/:id', handleUpdateUser);
+router.get('/api/testapi', testApi);
 
 export default router;
